ffccf804dcb134564753fe5e74ceb0c3f51c0afc
carlosnichele <97065715+carlosnichele@users.noreply.github.com>
2026-05-07 11:17:35 +0200

    Update transform.py

--- pipelines/transform.py
+++ pipelines/transform.py
@@ -1,1 +1,9 @@
+from utils.validation import validate_not_null
+from utils.helpers import timer
+
+@timer
+def transform_data(df):
+    df["value"] = df["value"].fillna(0)
+    validate_not_null(df, "value")
+    return df
 
